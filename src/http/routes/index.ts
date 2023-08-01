1acaf9068baa7fcaaac2a7c9ac15dcea5f741ede
gufronnakaaw <gufronnakaaw@gmail.com>
2023-08-02 05:27:37 +0700

    add: user routes

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import ErrorHandler from '../../handlers/ErrorHandler';
+import UserRoutes from '../../app/users/user.route';
 
 export default async function routes(fastify: FastifyInstance) {
+  fastify.register(UserRoutes, { prefix: 'users' });
   fastify.setErrorHandler(ErrorHandler);
 }
