6073e4117158b343414ab4f43f77f2f8a678b9f5
gufronnakaaw <gufronnakaaw@gmail.com>
2023-08-02 07:35:18 +0700

    create: test Login Users API

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, afterAll, beforeAll, expect } from '@jest/globals';
 import logger from '../utils/logger';
 import server from '../http/server';
-import { removeUsers } from './utils/user.util';
+import { createUsers, removeUsers } from './utils/user.util';
 
 describe('POST /api/v1/users/register', () => {
   beforeAll(async () => {
@@ -74,6 +74,121 @@ describe('POST /api/v1/users/register', () => {
         email: 'test@mail.com',
         fullname: '',
         password: '',
+      },
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.json()).toHaveProperty('success');
+    expect(response.json()).toHaveProperty('errors');
+
+    expect(response.json().success).toBeFalsy();
+    expect(response.json().errors).toBeDefined();
+
+    expect(response.json().errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          field: expect.any(String),
+          message: expect.any(String),
+        }),
+      ])
+    );
+  });
+});
+
+describe('POST /api/v1/users/login', () => {
+  beforeAll(async () => {
+    await createUsers();
+  });
+
+  afterAll(async () => {
+    await removeUsers();
+  });
+
+  it('should can login users', async () => {
+    const fastifyServer = server();
+
+    const response = await fastifyServer.inject({
+      method: 'POST',
+      url: '/api/v1/users/login',
+      payload: {
+        email: 'testing@mail.com',
+        password: 'testing123',
+      },
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.json()).toHaveProperty('success');
+    expect(response.json()).toHaveProperty('data');
+
+    expect(response.json().success).toBeTruthy();
+    expect(response.json().data).toEqual(
+      expect.objectContaining({
+        token: expect.any(String),
+      })
+    );
+  });
+
+  it('should cannot login users (wrong email)', async () => {
+    const fastifyServer = server();
+
+    const response = await fastifyServer.inject({
+      method: 'POST',
+      url: '/api/v1/users/login',
+      payload: {
+        email: 'wrong@mail.com',
+        password: 'testing123',
+      },
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.json()).toHaveProperty('success');
+    expect(response.json()).toHaveProperty('errors');
+
+    expect(response.json().success).toBeFalsy();
+    expect(response.json().errors).toBeDefined();
+
+    expect(response.json().errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ message: expect.any(String) }),
+      ])
+    );
+  });
+
+  it('should cannot login users (wrong password)', async () => {
+    const fastifyServer = server();
+
+    const response = await fastifyServer.inject({
+      method: 'POST',
+      url: '/api/v1/users/login',
+      payload: {
+        email: 'testing@mail.com',
+        password: 'wrongpassword',
+      },
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.json()).toHaveProperty('success');
+    expect(response.json()).toHaveProperty('errors');
+
+    expect(response.json().success).toBeFalsy();
+    expect(response.json().errors).toBeDefined();
+
+    expect(response.json().errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ message: expect.any(String) }),
+      ])
+    );
+  });
+
+  it('should cannot login users if request is invalid', async () => {
+    const fastifyServer = server();
+
+    const response = await fastifyServer.inject({
+      method: 'POST',
+      url: '/api/v1/users/login',
+      payload: {
+        email: 'invalid request',
+        password: 'ajshdjkashdjk',
       },
     });
 
--- src/tests/utils/user.util.ts
+++ src/tests/utils/user.util.ts
@@ -1,9 +1,20 @@
 import prisma from '../../utils/database';
+import { hash } from '../../utils/password';
 
 export async function removeUsers() {
   await prisma.user.deleteMany({
     where: {
       email: 'testing@mail.com',
+    },
+  });
+}
+
+export async function createUsers() {
+  await prisma.user.create({
+    data: {
+      email: 'testing@mail.com',
+      fullname: 'Testing',
+      password: await hash('testing123'),
     },
   });
 }
