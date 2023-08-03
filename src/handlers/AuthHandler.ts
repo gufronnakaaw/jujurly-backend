e83f6c1b58604a1ab6db883eae7612d1fc654514
gufronnakaaw <gufronnakaaw@gmail.com>
2023-08-03 14:15:29 +0700

    delete: done parameter

--- src/handlers/AuthHandler.ts
+++ src/handlers/AuthHandler.ts
@@ -1,17 +1,14 @@
-import { FastifyRequest, FastifyReply, HookHandlerDoneFunction } from 'fastify';
+import { FastifyRequest, FastifyReply } from 'fastify';
 import ResponseError from '../error/ResponseError';
 
 export default async function AuthHandler(
   req: FastifyRequest,
-  rep: FastifyReply,
-  done: HookHandlerDoneFunction
+  rep: FastifyReply
 ) {
   try {
     const { id }: { id: number } = await req.jwtVerify();
 
     req.user = { id };
-
-    done();
   } catch (error) {
     throw new ResponseError(401, 'Unauthorized');
   }
