39da161a983f77ecf2c483380ff2c8ae1d9cdad9
gufronnakaaw <gufronnakaaw@gmail.com>
2023-08-03 13:48:25 +0700

    add: room routes

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify';
 import ErrorHandler from '../../handlers/ErrorHandler';
 import UserRoutes from '../../app/users/user.route';
+import RoomRoutes from '../../app/rooms/room.route';
 
 export default async function routes(fastify: FastifyInstance) {
   fastify.register(UserRoutes, { prefix: 'users' });
+  fastify.register(RoomRoutes, { prefix: 'rooms' });
   fastify.setErrorHandler(ErrorHandler);
 }
