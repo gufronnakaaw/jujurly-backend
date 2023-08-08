e62d1f6f0f214d9eede5c1d09399fffc0db266db
gufronnakaaw <gufronnakaaw@gmail.com>
2023-08-08 09:55:08 +0700

    create: test Get Rooms By Id API

--- src/tests/room.test.ts
+++ src/tests/room.test.ts
@@ -409,3 +409,127 @@ describe('GET /api/v1/rooms', () => {
     );
   });
 });
+
+describe('GET BY ID /api/v1/rooms', () => {
+  beforeAll(async () => {
+    await createUsers();
+    await createRooms();
+  });
+
+  afterAll(async () => {
+    await deleteCandidates();
+    await deleteRooms();
+    await removeUsers();
+  });
+
+  it('should can get rooms by id', async () => {
+    const fastifyServer = server();
+    const token = await doLogin();
+    const room = await getRooms();
+
+    const response = await fastifyServer.inject({
+      method: 'GET',
+      url: `/api/v1/rooms?id=${room?.id}`,
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.json()).toHaveProperty('success');
+    expect(response.json()).toHaveProperty('data');
+
+    expect(response.json().success).toBeTruthy();
+    expect(response.json().data).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        name: expect.any(String),
+        start: expect.any(Number),
+        end: expect.any(Number),
+        code: expect.any(String),
+        candidates: expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(Number),
+            name: expect.any(String),
+          }),
+        ]),
+      })
+    );
+  });
+
+  it('should cannot get rooms by id if room not found', async () => {
+    const fastifyServer = server();
+    const token = await doLogin();
+    const room = await getRooms();
+
+    const response = await fastifyServer.inject({
+      method: 'GET',
+      url: `/api/v1/rooms?id=${room!.id + 1}`,
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    expect(response.statusCode).toBe(404);
+    expect(response.json()).toHaveProperty('success');
+    expect(response.json()).toHaveProperty('errors');
+
+    expect(response.json().success).toBeFalsy();
+    expect(response.json().errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          message: expect.any(String),
+        }),
+      ])
+    );
+  });
+
+  it('should unauthorized', async () => {
+    const fastifyServer = server();
+    const room = await getRooms();
+
+    const response = await fastifyServer.inject({
+      method: 'GET',
+      url: `/api/v1/rooms?id=${room!.id}`,
+    });
+
+    expect(response.statusCode).toBe(401);
+    expect(response.json()).toHaveProperty('success');
+    expect(response.json()).toHaveProperty('errors');
+
+    expect(response.json().success).toBeFalsy();
+    expect(response.json().errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          message: expect.any(String),
+        }),
+      ])
+    );
+  });
+
+  it('should cannot get rooms by id if request invalid', async () => {
+    const fastifyServer = server();
+    const token = await doLogin();
+
+    const response = await fastifyServer.inject({
+      method: 'GET',
+      url: `/api/v1/rooms?id=wrong`,
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.json()).toHaveProperty('success');
+    expect(response.json()).toHaveProperty('errors');
+
+    expect(response.json().success).toBeFalsy();
+    expect(response.json().errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          message: expect.any(String),
+        }),
+      ])
+    );
+  });
+});
